167bdda59dc90d96e3ea651a238806720f06c32c
tellyworth <alex@automattic.com>
2024-04-16 12:27:45 +1000

    Update tests now that the block converter is working better

--- phpunit/tests/test-block-converter.php
+++ phpunit/tests/test-block-converter.php
@@ -24,11 +24,19 @@ class TestBlockConverter extends \PHPUnit\Framework\TestCase {
 		</div>
 EOF;
 
+		$expected = <<<EOF
+<!-- wp:columns {"layout":{"type":"flex"},"align":"wide"} -->
+<div class="wp-block-columns alignwide is-layout-flex wp-container-core-columns-is-layout-1 wp-block-columns-is-layout-flex"><!-- wp:column {"layout":{"type":"default"}} --><div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:50%"><!-- wp:heading {"level":1} --><h1 class="wp-block-heading" style="font-size:70px">Meet WordPress</h1><!-- /wp:heading -->
+<!-- wp:paragraph --><p class="is-style-short-text">The open source publishing platform of choice for millions of websites worldwide—from creators and small businesses to enterprises.</p><!-- /wp:paragraph --></div><!-- /wp:column -->
+<!-- wp:column {"layout":{"type":"default"}} --><div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:50%"><!-- wp:html --><figure class="wp-block-image size-full"><img width="1198" height="323" src="https://wordpress.org/files/2024/04/brush.png" alt="" class="wp-image-39758" srcset="https://i0.wp.com/wordpress.org/files/2024/04/brush.png?w=1198&amp;ssl=1 1198w, https://i0.wp.com/wordpress.org/files/2024/04/brush.png?resize=300%2C81&amp;ssl=1 300w, https://i0.wp.com/wordpress.org/files/2024/04/brush.png?resize=1024%2C276&amp;ssl=1 1024w, https://i0.wp.com/wordpress.org/files/2024/04/brush.png?resize=768%2C207&amp;ssl=1 768w" sizes="(max-width: 1198px) 100vw, 1198px"></figure><!-- /wp:html --></div><!-- /wp:column --></div>
+<!-- /wp:columns -->
+EOF;
+
 		$converter = new Block_Converter_Recursive( $html );
 
 		$blocks = $converter->convert();
-		var_dump( __METHOD__, $html, $blocks );ob_flush();flush();
-		$this->assertNotEmpty( $blocks );
+		#var_dump( __METHOD__, $html, $blocks );ob_flush();flush();
+		$this->assertEquals( $expected, $blocks );
 	}
 
 	public function test_sample_html2() {
@@ -36,11 +44,18 @@ EOF;
 		<p class="is-style-short-text">The open source publishing platform of choice for millions of websites worldwide—from creators and small businesses to enterprises.</p>
 EOF;
 
+		// FIXME: the class should correspond with a block attribute probably
+		$expected = <<<EOF
+<!-- wp:paragraph -->
+<p class="is-style-short-text">The open source publishing platform of choice for millions of websites worldwide—from creators and small businesses to enterprises.</p>
+<!-- /wp:paragraph -->
+EOF;
+
 		$converter = new Block_Converter_Recursive( $html );
 
 		$blocks = $converter->convert();
-		var_dump( __METHOD__, $html, $blocks );ob_flush();flush();
-		$this->assertNotEmpty( $blocks );
+		#var_dump( __METHOD__, $html, $blocks );ob_flush();flush();
+		$this->assertEquals( $expected, $blocks );
 	}
 
 	public function test_div() {
