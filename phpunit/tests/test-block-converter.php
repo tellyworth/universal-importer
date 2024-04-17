7cbcc4d391feb9f1b78d7cb1596e5a1e5934abc6
tellyworth <alex@automattic.com>
2024-04-17 11:37:36 +1000

    Hide debug

--- phpunit/tests/test-block-converter.php
+++ phpunit/tests/test-block-converter.php
@@ -59,7 +59,7 @@ EOF;
 		$converter = new Block_Converter_Recursive( $html );
 
 		$blocks = $converter->convert();
-		var_dump( __METHOD__, 'input', $html, 'actual', $blocks );ob_flush();flush();
+		#var_dump( __METHOD__, 'input', $html, 'actual', $blocks );ob_flush();flush();
 		$this->assertEquals( $expected, $blocks );
 	}
 
