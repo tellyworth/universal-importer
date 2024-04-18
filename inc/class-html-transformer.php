ca2f8fce93d8aa921790558bbd973049331f2f0a
tellyworth <alex@automattic.com>
2024-04-18 17:24:01 +1000

    Remove old code

--- inc/class-html-transformer.php
+++ inc/class-html-transformer.php
@@ -64,53 +64,6 @@ class HTMLTransformer {
 	}
 
 	private function init() {
-		/*if ( false && function_exists( 'gutenberg_get_layout_definitions' ) ) {
-			if ( $layouts = \gutenberg_get_layout_definitions() ) {
-				foreach ( $layouts as $name => $layout ) {
-					$this->layout_classes[ $layout['className'] ] = $name;
-				}
-			}
-		} else {*/
-		//}
-/*
-		#Block_Converter::macro( 'div', array( $this, 'convert_div') );
-		Block_Converter::macro( 'div', function( \DOMNode $node ) {
-			#var_dump( 'div macro', get_called_class() ); flush(); ob_flush();
-			var_dump( 'checking div with class', $node->getAttribute('class') ); flush(); ob_flush();
-			$content = Block_Converter::get_node_html( $node );
-
-			#$content = $node->textContent;
-			#$content = null;
-			// Note that `self` would refer here to Block_Converter, not HTMLTransformer.
-			// FIXME: this is incomplete and should go in a helper function so it can be reused for other block types.
-			$atts = [];
-			if ( $layout = HTMLTransformer::get_node_layout_name( $node ) ) {
-				#var_dump( "got layout", $layout );
-				$atts['layout'] = [ 'type' => $layout ];
-			}
-			if ( HTMLTransformer::node_has_class( $node, 'alignwide' ) ) {
-				$atts['align'] = 'wide';
-			}
-			if ( HTMLTransformer::node_has_class( $node, 'alignfull' ) ) {
-				$atts['align'] = 'full';
-			}
-			if ( HTMLTransformer::node_has_class( $node, 'has-medium-font-size' ) ) {
-				$atts['fontSize'] = 'medium';
-			}
-			if ( HTMLTransformer::node_has_class( $node, 'wp-block-columns' ) ) {
-				var_dump( "core/columns", $atts, $content ); flush(); ob_flush();
-				return new Block( 'core/columns', $atts, $content );
-			} elseif ( HTMLTransformer::node_has_class( $node, 'wp-block-column' ) ) {
-				var_dump( "core/column", $atts, $content ); flush(); ob_flush();
-				return new Block( 'core/column', $atts, $content );
-			}
-
-			var_dump( "default div" ); flush(); ob_flush();
-			// FIXME: this should be a div; need to double check it's not causing an infinite loop like early bugs.
-			return Block_Converter::html( $node );
-		} );
-
-	*/
 	}
 
 	/**
