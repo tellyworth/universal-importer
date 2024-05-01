616c7d3d6cdb1088abf85d5a5eeb79c48dd67dc6
tellyworth <alex@automattic.com>
2024-05-01 13:58:56 +1000

    Simple UI for importing a single page

--- inc/class-universal-importer.php
+++ inc/class-universal-importer.php
@@ -33,42 +33,51 @@ class Universal_Importer {
 	}
 
 	public function import( $url, $handler = null, $media_handler = null ) {
+		$this->page_handler = $handler;
+		$this->media_handler = $media_handler;
+
 		$site_indexer = SiteIndexer::instance();
 		$sitemaps = $site_indexer->get_sitemaps( $url );
 
 		if ( empty( $sitemaps ) ) {
 			return [];
 		}
-		$page_fetcher = PageFetcher::instance();
-		$page_traverser = PageTraverser::instance();
-		$html_transformer = HTMLTransformer::instance();
+
 
 		$pages = $site_indexer->get_urls();
 
 		foreach ( $pages as $url ) {
-			$html = $page_fetcher->fetch( $url );
-			$page_traverser->parse_content( $html );
-
-			// Process internal media URLs before we start fetching content, so we can rewrite URLs to local attachments.
-			$this->internal_media_urls = $page_traverser->get_internal_media_urls();
-			if ( $this->internal_media_urls && is_callable( $media_handler ) ) {
-				call_user_func( $media_handler, $this->internal_media_urls, $page_traverser );
-			}
-
-			$this->navigation = $page_traverser->get_navigation();
-
-			$this->last_page_content = $page_traverser->get_content();
-
-			if ( $this->last_page_content ) {
-				$blocks = $html_transformer->transform( $this->last_page_content );
-
-				if ( $handler && is_callable( $handler ) ) {
-					call_user_func( $handler, $url, $blocks, $page_traverser );
-				}
-
-			}
-			#return; // FIXME short circuit for now
+			$this->import_page( $url, $handler, $media_handler );
 		}
 
+	}
+
+	public function import_page( $url, $handler, $media_handler ) {
+
+		$page_fetcher = PageFetcher::instance();
+		$page_traverser = PageTraverser::instance();
+		$html_transformer = HTMLTransformer::instance();
+
+		$html = $page_fetcher->fetch( $url );
+		$page_traverser->parse_content( $html );
+
+		// Process internal media URLs before we start fetching content, so we can rewrite URLs to local attachments.
+		$this->internal_media_urls = $page_traverser->get_internal_media_urls();
+		if ( $this->internal_media_urls && is_callable( $media_handler ) ) {
+			call_user_func( $media_handler, $this->internal_media_urls, $page_traverser );
+		}
+
+		$this->navigation = $page_traverser->get_navigation();
+
+		$this->last_page_content = $page_traverser->get_content();
+
+		if ( $this->last_page_content ) {
+			$blocks = $html_transformer->transform( $this->last_page_content );
+
+			if ( $handler && is_callable( $handler ) ) {
+				call_user_func( $handler, $url, $blocks, $page_traverser );
+			}
+
+		}
 	}
 }
